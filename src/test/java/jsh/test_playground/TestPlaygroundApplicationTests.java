02fcf1f214a54b424081c99aca6844a1010e5622
Seonghun Jeong <tjdgns5506@gmail.com>
2024-11-12 22:30:56 +0900

    chore: 초기 세팅

---
+++ src/test/java/jsh/test_playground/TestPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package jsh.test_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
